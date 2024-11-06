eac9a4e5145121adeb441f0e4cc5c96cf021df53
Gaizka Carmona <gcarmona002@ikasle.ehu.eus>
2024-11-06 14:07:29 +0100

    Time configuration but failed, same as before

--- app/includes/dbConnect.php
+++ app/includes/dbConnect.php
@@ -1,16 +1,17 @@
 <?php
-$config = include('config.php');
+    date_default_timezone_set('Europe/Madrid');
+    $config = include('config.php');
 
-$hostname = $config['hostname'];
-$username = $config['username'];
-$password = $config['password'];
-$database = $config['database'];
+    $hostname = $config['hostname'];
+    $username = $config['username'];
+    $password = $config['password'];
+    $database = $config['database'];
 
-// Create connection
-$conn = mysqli_connect($hostname, $username, $password, $database);
+    // Create connection
+    $conn = mysqli_connect($hostname, $username, $password, $database);
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 ?>
