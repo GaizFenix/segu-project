6292f0d47db4e6e82d92c7a00a2eb6b5f7157dc4
Gaizka Carmona <gcarmona002@ikasle.ehu.eus>
2024-11-21 11:05:34 +0100

    IDK what i'm doing with my life at this point

---
+++ app/includes/config.php
@@ -0,0 +1,10 @@
+<?php
+    return [
+        'hostname' => 'db',
+        'username' => 'Anonymous',
+        'password' => 'uI562&28',
+        'database' => 'database',
+        'siteKey' => '6Le0o4IqAAAAADFei4ZLfoFl0htszdYmz-8FhFf-',
+        'secretKey' => '6Le0o4IqAAAAACtCsim0lPH5872duGvgqEbyQYvC'
+    ];
+?>
